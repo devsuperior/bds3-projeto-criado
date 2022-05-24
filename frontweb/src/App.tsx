21ddd0be44ae7c4c846aef1c0b8f07f372ff70bf
Nelio Alves <acenelio@gmail.com>
2022-05-24 12:07:40 -0300

    Project created

---
+++ frontweb/src/App.tsx
@@ -0,0 +1,11 @@
+import Hello from 'components/Hello';
+import './App.css';
+
+function App() {
+
+  return (
+    <Hello />
+  );
+}
+
+export default App;
---
+++ frontweb/src/components/Hello/index.tsx
@@ -0,0 +1,8 @@
+function Hello() {
+
+    return (
+        <h1>Ola mundo</h1>
+    );
+}
+
+export default Hello;
